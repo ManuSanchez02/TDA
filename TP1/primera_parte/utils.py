b11f1b84d92070dc400c848492b5f50d640cad3f
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-01 16:08:15 -0300

    Replaced complexity analysis with notebook

--- TP1/primera_parte/utils.py
+++ TP1/primera_parte/utils.py
@@ -1,5 +1,4 @@
 import random
 
-def random_sorted_array(n, seed=None):
-    random.seed(seed)
-    return sorted(random.randint(0, 1000) for _ in range(n))
+def random_sorted_array(n):
+    return sorted(random.randint(0, 10000) for _ in range(n))
