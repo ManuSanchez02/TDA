1107ef90540ec82d2e5e2f91fc84346ede9c71b1
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-25 19:42:35 -0300

    Add simultaneous tests for kmerge

--- TP1/primera_parte/tests.py
+++ TP1/primera_parte/tests.py
@@ -4,11 +4,11 @@ from utils import random_sorted_array
 # -------------------------------
 # |    COMMENT ONE OF THESE:    |
 # -------------------------------
-# from kmerge_dyc import kmerge
-from kmerge_heap import kmerge
+from kmerge_dyc import kmerge as kmerge_dyc
+from kmerge_heap import kmerge as kmerge_heap
 
 
-class TestKMerge(unittest.TestCase):
+class TestKMergeDyC(unittest.TestCase):
 
     def setUp(self):
         self.array1 = random_sorted_array(10)
@@ -17,24 +17,53 @@ class TestKMerge(unittest.TestCase):
 
     def test_merge_empty_array(self):
         self.assertEqual(
-            kmerge([]), [], 'Merge of empty array returns empty array')
+            kmerge_dyc([]), [], 'Merge of empty array returns empty array')
 
     def test_merge_empty_arrays(self):
-        self.assertEqual(kmerge([], []), [],
+        self.assertEqual(kmerge_dyc([], []), [],
                          'Merge of empty arrays returns empty array')
 
     def test_merge_one_array(self):
-        self.assertEqual(kmerge(self.array1), self.array1,
+        self.assertEqual(kmerge_dyc(self.array1), self.array1,
                          'Merge of one arrays returns that array')
 
     def test_merge_two_arrays(self):
-        self.assertEqual(kmerge(self.array1, self.array2), sorted(self.array1+self.array2),
+        self.assertEqual(kmerge_dyc(self.array1, self.array2), sorted(self.array1+self.array2),
                          'Merge of 2 arrays returns a merge of them')
     
     def test_merge_multiple_arrays(self):
         arrays = [random_sorted_array(10) for i in range(15)]
         expected = sorted([n for array in arrays for n in array])
-        self.assertEqual(kmerge(*arrays), expected,
+        self.assertEqual(kmerge_dyc(*arrays), expected,
+                         'Merge of 2 arrays returns a merge of them')
+        
+class TestKMergeHeaps(unittest.TestCase):
+
+    def setUp(self):
+        self.array1 = random_sorted_array(10)
+        self.array2 = random_sorted_array(10)
+        return super().setUp()
+
+    def test_merge_empty_array(self):
+        self.assertEqual(
+            kmerge_heap([]), [], 'Merge of empty array returns empty array')
+
+    def test_merge_empty_arrays(self):
+        self.assertEqual(kmerge_heap([], []), [],
+                         'Merge of empty arrays returns empty array')
+
+    def test_merge_one_array(self):
+        self.assertEqual(kmerge_heap(self.array1), self.array1,
+                         'Merge of one arrays returns that array')
+
+    def test_merge_two_arrays(self):
+        self.assertEqual(kmerge_heap(self.array1, self.array2), sorted(self.array1+self.array2),
+                         'Merge of 2 arrays returns a merge of them')
+    
+    def test_merge_multiple_arrays(self):
+        arrays = [random_sorted_array(10) for i in range(15)]
+        expected = sorted([n for array in arrays for n in array])
+        self.assertEqual(kmerge_heap(*arrays), expected,
                          'Merge of 2 arrays returns a merge of them')
 
 
