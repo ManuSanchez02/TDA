6b32c4297c4cc72607ad73ac89221783352591d3
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-31 00:07:06 -0300

    Add initial files

---
+++ TP2/main.py
@@ -0,0 +1,11 @@
+
+def empaquetar(objetos: list, n: int = 1):
+    solucion = []
+
+    return solucion
+
+
+if __name__ == '__main__':
+    objetos = [0.4, 0.2, 0.5, 0.3, 0.7, 0.6, 0.1, 0.4, 0.2, 0.2]
+    solucion = empaquetar(objetos)
+    print(solucion)
